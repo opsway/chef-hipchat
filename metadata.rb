e77bd655502a260cf13e3e31f365b64ca86b9033
Andriy Samilyak <samilyak@gmail.com>
2013-04-01 14:35:22 +0300

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             "hipchat"
+maintainer       "Andriy Samilyak"
+maintainer_email "samilyak@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures hipchat"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends "chef_handler"
+
---
+++ recipes/default.rb
@@ -0,0 +1,28 @@
+include_recipe "chef_handler"
+
+# Install the hipchat notification gem
+gem_package "hipchat"
+
+Gem.clear_paths
+
+hipchat_secrets = Chef::DataBagItem.load('secrets', 'hipchat')
+
+# Install the handler to the handlers folder
+template "#{node['chef_handler']['handler_path']}/hipchat-notification-handler.rb" do
+  source "hipchat-notification-handler.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  variables(
+    :api_token => hipchat_secrets['api_token'],
+    :room_name => hipchat_secrets['room_name']
+  )
+end
+
+# Configure the handler
+chef_handler "HipChat::NotifyRoom" do
+  source "#{node['chef_handler']['handler_path']}/hipchat-notification-handler.rb"
+  supports :exception => true
+  arguments :notify_users => false
+  action :enable
+end
