e4f7ac529d3bac1b27a1a20ccd69f3e4caf4c741
Andriy Samilyak <samilyak@gmail.com>
2013-04-02 14:23:06 +0300

    Recipe to add a command to /usr/bin to send message from command line

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email "samilyak@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures hipchat"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 depends "chef_handler"
+depends "ruby"
 
---
+++ recipes/command.rb
@@ -0,0 +1,20 @@
+#Install /usr/bin script that takes message as a parameter and sends it to HipChat room
+
+include_recipe "ruby"
+
+# Install the hipchat notification gem
+gem_package "hipchat"
+
+hipchat_secrets = Chef::DataBagItem.load('secrets', 'hipchat')
+
+# Install the handler to the handlers folder
+template "/usr/bin/hipchat" do
+  source "hipchat-command.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+  variables(
+    :api_token => hipchat_secrets['api_token'],
+    :room_name => hipchat_secrets['room_name']
+  )
+end
