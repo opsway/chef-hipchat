6fbbf423deb540b73c4481b5013e798fe10793e6
Andriy Samilyak <samilyak@gmail.com>
2013-12-07 18:16:59 +0200

    Build-essential included in compile_time

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "samilyak@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures hipchat"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
+version          "0.2.2"
 
 depends "chef_handler"
 depends "ruby"
